686d909add2b462689d8bf36e0666477f5fb19e7
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 16:03:02 +0530

    Poco Class added

---
+++ EFFluentAPIWithSeedingDataDemo/ClsEmployee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFFluentAPIWithSeedingDataDemo
+{
+    internal class ClsEmployee
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmpID { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "Please Enter First Name e.g. John")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Last Name e.g. Doe")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Password should not be blank")]
+        public string Password { get; set; }
+
+
+        [DisplayName("Cell Number")] // Form level as we cannot enter sapce in column name in DB it only display in c sharp
+        [Required(ErrorMessage = "Cell Number should not be blank")]
+        public string CellNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Enter valid email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [NotMapped]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; } // this is only in your application or in class 
+
+        public virtual ICollection<ClsSkill> ClsSkills { get; set; }
+    }
+}
---
+++ EFFluentAPIWithSeedingDataDemo/ClsSkill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFFluentAPIWithSeedingDataDemo
+{
+    internal class ClsSkill
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SkillId { get; set; }
+        [ForeignKey("ClsEmployee")]
+
+        public int EmpID { get; set; }
+
+
+        [DisplayName("Skill Name")]
+        [Required(ErrorMessage = "Skill name should not be blank")]
+
+        public string SkillName { get; set; }
+        [Required(ErrorMessage = "Employee Role should not be blank")]
+        [DisplayName("Role")]
+        public string Role { get; set; }
+        [DisplayName("Experience In Years")]
+        [Required(ErrorMessage = "Experience in years should not be blank")]
+        public int ExperienceInYears { get; set; }
+        public virtual ClsEmployee ClsEmployee { get; set; }
+    }
+}
