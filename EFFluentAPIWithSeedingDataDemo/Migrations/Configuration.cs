8bca5284ecb2c68056af87a7266382bb290eaa45
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 16:41:21 +0530

    records added data which we have to insert into database added in configuration.cs

--- EFFluentAPIWithSeedingDataDemo/Migrations/Configuration.cs
+++ EFFluentAPIWithSeedingDataDemo/Migrations/Configuration.cs
@@ -18,7 +18,17 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            context.tblEmployees.AddOrUpdate(
+                      new ClsEmployee { EmpID = 1, FirstName = "Aaron", LastName = "Hawkins", Password = "arron@123", CellNumber = "(660) 663-4518", Email = "aron.hawkins@aol.com" },
+                       new ClsEmployee { EmpID = 2, FirstName = "Hedy", LastName = "Greene", Password = "hedy@123", CellNumber = "(608) 265-2215", Email = "hedy.greene@aol.com" },
+                       new ClsEmployee { EmpID = 3, FirstName = "Melvin", LastName = "Porter", Password = "melvin@123", CellNumber = "(959) 119-8364", Email = "melvin.porter@aol.com" }
+                      );
 
+            context.tblSkills.AddOrUpdate(
+            new ClsSkill { SkillId = 1, EmpID = 1, SkillName = "Microsoft Office Suite", Role = "Business Analyst", ExperienceInYears = 2 },
+            new ClsSkill { SkillId = 2, EmpID = 1, SkillName = "Testing", Role = "Developer", ExperienceInYears = 3 },
+            new ClsSkill { SkillId = 3, EmpID = 1, SkillName = "Stakeholder Management", Role = "Project Lead", ExperienceInYears = 4 }
+            );
 
         }
     }
