e7731135b4e560cc7c37d2f323e4d0eac90d52a9
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 16:39:38 +0530

    Migartion Enabled

---
+++ EFFluentAPIWithSeedingDataDemo/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace EFFluentAPIWithSeedingDataDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EFFluentAPIWithSeedingDataDemo.AppDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EFFluentAPIWithSeedingDataDemo.AppDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+
+        }
+    }
+}
