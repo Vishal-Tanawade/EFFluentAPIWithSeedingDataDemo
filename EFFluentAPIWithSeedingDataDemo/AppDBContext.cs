b42fd9f6446eda722502aeaa3689370f71c0bf6e
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 16:05:10 +0530

    DbContext class created and connection string added

---
+++ EFFluentAPIWithSeedingDataDemo/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFFluentAPIWithSeedingDataDemo
+{
+    internal class AppDBContext : DbContext
+    {
+        public AppDBContext() : base("name=EmployeeSkillConnnectionString")
+        {
+
+        }
+
+
+        public DbSet<ClsEmployee> tblEmployees { get; set; }
+        public DbSet<ClsSkill> tblSkills { get; set; }
+    }
+}
